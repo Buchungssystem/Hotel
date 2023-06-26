a53be140064bfb87251572fd07b0eb3bcb121d4f
Nico1808 <niconiebisch@gmail.com>
2023-06-26 21:51:47 +0200

    japi

---
+++ src/main/java/org/hotel/Execute.java
@@ -0,0 +1,11 @@
+package org.hotel;
+
+public class Execute {
+    public static void main(String[] args) {
+        Hotel hotel = new Hotel();
+
+        while(true){
+
+        }
+    }
+}
--- src/main/java/org/hotel/Hotel.java
+++ src/main/java/org/hotel/Hotel.java
@@ -1,7 +1,24 @@
 package org.hotel;
 
 import org.utils.Participant;
+import org.utils.VoteOptions;
+
+import java.time.LocalDate;
 
 public class Hotel extends Participant {
 
+    @Override
+    public VoteOptions Vote() {
+        return VoteOptions.ABORT;
+    }
+
+    @Override
+    public void book() {
+
+    }
+
+    @Override
+    public void getAvailableItems(LocalDate startDate, LocalDate endDate) {
+
+    }
 }
