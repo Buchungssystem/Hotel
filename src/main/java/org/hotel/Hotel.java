203f002f5e5c1f16c3f2641e9964a3078d88e033
Nico1808 <niconiebisch@gmail.com>
2023-07-03 22:06:39 +0200

    justinggggg

--- src/main/java/org/hotel/Hotel.java
+++ src/main/java/org/hotel/Hotel.java
@@ -5,10 +5,7 @@ import org.utils.Operations;
 import org.utils.Participant;
 import org.utils.SendingInformation;
 import org.utils.UDPMessage;
-import org.utils.Operations;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -24,18 +21,17 @@ public class Hotel extends Participant {
     }
 
     @Override
-    public void book() {
+    public byte[] book(LocalDate startDate, LocalDate endDate) {
 
     }
 
+
     @Override
-    public byte[] getAvailableItems(LocalDate startDate, LocalDate endDate, UUID pTransaktionnumber) {
+    public ArrayList<Object> getAvailableItems(LocalDate startDate, LocalDate endDate) {
         DatabaseConnection dbConn = new DatabaseConnection();
         ArrayList<String> availableRoomIds = new ArrayList<>();
         ResultSet rs = null;
-        ObjectMapper objectMapper = new ObjectMapper();
         UDPMessage udpMessage;
-        byte[] data;
         try(Connection con = dbConn.getConn()){
             PreparedStatement stm = con.prepareStatement("SELECT * FROM booking");
             rs = stm.executeQuery();
@@ -62,27 +58,22 @@ public class Hotel extends Participant {
             rs = stm.executeQuery();
             ArrayList<Room> availableRooms = new ArrayList<>();
             while(rs.next()){
-                availableRooms.add(new Room(rs.getInt("id"), rs.getString("name"), rs.getDouble("price")));
+                availableRooms.add(new Room(rs.getInt("id"), rs.getString("name")));
             }
 
-            data = objectMapper.writeValueAsBytes(availableRooms);
-            udpMessage = new UDPMessage(pTransaktionnumber, data, SendingInformation.HOTEL, Operations.AVAILIBILITY);
-
-            return objectMapper.writeValueAsBytes(udpMessage);
+            return availableRooms;
 
         }catch(Exception e){
-            String errorMessage = "Something went wrong:\n" + e.getMessage();
-            byte[] res;
-            try {
-                data = objectMapper.writeValueAsBytes(errorMessage);
-                udpMessage = new UDPMessage(pTransaktionnumber, data, SendingInformation.HOTEL, Operations.AVAILIBILITY);
-                res = objectMapper.writeValueAsBytes(udpMessage);
-            }catch (com.fasterxml.jackson.core.JsonProcessingException er){
-                return errorMessage.getBytes();
-            }
-
-            return res;
+            System.out.println("An Error has occured: " + e.getMessage());
+            return null;
         }
+    }
+
+    public boolean isAvailable(LocalDate startDate, LocalDate endDate, LocalDate startDateEntry, LocalDate endDateEntry){
+        if (!(startDateEntry.isAfter(endDate) | endDateEntry.isBefore(startDate))) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
