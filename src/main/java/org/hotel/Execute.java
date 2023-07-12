82992deff9d8ab8388ec52fda4369d6eeab67700
Nico1808 <niconiebisch@gmail.com>
2023-07-12 12:59:33 +0200

    last safe

--- src/main/java/org/hotel/Execute.java
+++ src/main/java/org/hotel/Execute.java
@@ -1,9 +1,11 @@
 package org.hotel;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.utils.*;
 
+import javax.xml.crypto.Data;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -27,30 +29,68 @@ public class Execute {
 
         while (true) {
             try (DatagramSocket dgSocket = new DatagramSocket(Participant.hotelPort)) {
+                //byte data of UDP message
+                byte[] messageData;
+
+                //byte UDP Message
+                byte [] parsedMessage;
                 byte[] buffer = new byte[65507];
                 //DatagramPacket for recieving data
                 DatagramPacket dgPacketIn = new DatagramPacket(buffer, buffer.length);
+                //response UDPMessage
+                UDPMessage responeseMessage;
 
                 System.out.println("Listening on Port " + Participant.hotelPort);
                 dgSocket.receive(dgPacketIn);
+                System.out.println("recieved aber trd im arsch: ");
+
+                //string data to parse it into Objects
                 String data = new String(dgPacketIn.getData(), 0, dgPacketIn.getLength());
+                //parsed UDP message
                 UDPMessage dataObject = objectMapper.readValue(data, UDPMessage.class);
+                System.out.println(dataObject.getOperation());
 
                 switch (dataObject.getOperation()){
                     case PREPARE -> {
+                        System.out.println("prepare recieved");
+                        messageData = dataObject.getData();
+                        data = new String(messageData, 0, messageData.length);
+                        BookingData bookingData = objectMapper.readValue(data, BookingData.class);
 
+                        Operations response = h.prepare(bookingData, dataObject.getTransaktionNumber());
+                        responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), new byte[0], SendingInformation.HOTEL, response);
+
+                        //send response back to TravelBroker
+                        messageData = objectMapper.writeValueAsBytes(responeseMessage);
+                        DatagramPacket dgOutPrepare = new DatagramPacket(messageData, messageData.length, Participant.localhost, Participant.travelBrokerPort);
+
+                        dgSocket.send(dgOutPrepare);
+                        System.out.println("prepare answered: " + response);
                     }
                     case COMMIT -> {
+                        System.out.println("commit recieved");
+                        if(h.commit(dataObject.getTransaktionNumber())) {
+                            responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), new byte[0], SendingInformation.HOTEL, Operations.OK);
+                            parsedMessage = objectMapper.writeValueAsBytes(responeseMessage);
+                            DatagramPacket dgOutCommit = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, Participant.travelBrokerPort);
 
+                            dgSocket.send(dgOutCommit);
+                            System.out.println("commit answered - Ok");
+                        }
                     }
                     case ABORT -> {
+                        System.out.println("abort recieved");
+                        if(h.abort(dataObject.getTransaktionNumber())){
+                            responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), new byte[0], SendingInformation.HOTEL, Operations.OK);
+                            parsedMessage = objectMapper.writeValueAsBytes(responeseMessage);
+                            DatagramPacket dgOutAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, Participant.travelBrokerPort);
 
-                    }
-                    case READY -> {
-
+                            dgSocket.send(dgOutAbort);
+                            System.out.println("abort answered - OK");
+                        }
                     }
                     case AVAILIBILITY -> {
-                        byte[] messageData = dataObject.getData();
+                        messageData = dataObject.getData();
                         AvailabilityData availabilityData = objectMapper.readValue(messageData, AvailabilityData.class);
 
                         ArrayList<Object> availableItems = h.getAvailableItems(availabilityData.getStartDate(), availabilityData.getEndDate());
@@ -58,11 +98,10 @@ public class Execute {
                         if(!(availableItems == null)){
                             byte[] parsedItems = objectMapper.writeValueAsBytes(availableItems);
                             UDPMessage responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), parsedItems, SendingInformation.HOTEL, Operations.AVAILIBILITY);
-                            byte [] parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
+                            parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
                             //Datagrampacket for sending the response
-                            DatagramPacket dgPacketOut = new DatagramPacket(parsedMessage, parsedMessage.length, h.localhost, h.travelBrokerPort);
-                            System.out.println("hotel hat gesendet");
-                            dgSocket.send(dgPacketOut);
+                            DatagramPacket dgOutAvailability= new DatagramPacket(parsedMessage, parsedMessage.length, h.localhost, h.travelBrokerPort);
+                            dgSocket.send(dgOutAvailability);
 
                         }
                     }
--- src/main/java/org/hotel/Hotel.java
+++ src/main/java/org/hotel/Hotel.java
@@ -1,8 +1,10 @@
 package org.hotel;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
 import org.utils.*;
 
+import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,15 +13,69 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Hotel extends Participant {
-
     @Override
-    public Operations Vote() {
-        return Operations.ABORT;
+    public Operations prepare(BookingData bookingData, UUID transaktionId) {
+        DatabaseConnection dbConn = new DatabaseConnection();
+        LocalDate startDate = bookingData.getStartDate();
+        LocalDate endDate = bookingData.getEndDate();
+        int requestedId = bookingData.getSelectedRoom();
+
+        try(Connection con = dbConn.getConn()) {
+
+            //Check if there is already any Booking over this Item
+            PreparedStatement stm = con.prepareStatement("SELECT * FROM booking");
+            ResultSet rs = stm.executeQuery();
+
+            while (rs.next()) {
+                if(rs.getInt("roomId") == requestedId){
+                    LocalDate startDateEntry = LocalDate.parse(rs.getString("startDate"));
+                    LocalDate endDateEntry = LocalDate.parse(rs.getString("endDate"));
+                    if (!isAvailable(startDate, endDate, startDateEntry, endDateEntry)) {
+                        return Operations.ABORT;
+                    }
+
+                }
+            }
+            //Room is available
+            String query = "INSERT INTO booking VALUES (\"" + transaktionId + "\", \"" + startDate + "\", \"" + endDate + "\", 0, " + requestedId + ")";
+            System.out.println(query);
+            stm = con.prepareStatement(query);
+            stm.executeUpdate();
+            System.out.println("successfully booked");
+
+            return Operations.READY;
+        }catch (Exception e){
+            System.out.println("kapput");
+            return Operations.ABORT;
+        }
+
     }
 
     @Override
-    public byte[] book(LocalDate startDate, LocalDate endDate) {
-        return new byte[0];
+    public boolean commit(UUID transaktionId){
+        DatabaseConnection dbConn = new DatabaseConnection();
+        try(Connection con = dbConn.getConn()){
+            PreparedStatement stm = con.prepareStatement("UPDATE booking SET stable = 1 WHERE id = \"" + transaktionId + "\"");
+            System.out.println("UPDATE booking SET stable = 1 WHERE id = \"" + transaktionId + "\"");
+            stm.executeUpdate();
+            return true;
+        }catch(Exception e){
+            System.out.println("des kann jetzt nicht Wahrsteiner im Commit?: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean abort(UUID transaktionId){
+        DatabaseConnection dbConn = new DatabaseConnection();
+        try(Connection con = dbConn.getConn()){
+            PreparedStatement stm = con.prepareStatement("DELETE FROM booking WHERE id = \"" + transaktionId + "\"");
+            stm.executeQuery();
+            return true;
+        }catch(Exception e){
+            System.out.println("des kann jetzt nicht Wahrsteiner im Abort?");
+            return false;
+        }
     }
 
 
@@ -36,7 +92,7 @@ public class Hotel extends Participant {
             while(rs.next()){
                 LocalDate startDateEntry = LocalDate.parse(rs.getString("startDate"));
                 LocalDate endDateEntry = LocalDate.parse(rs.getString("endDate"));
-                if (!(startDateEntry.isAfter(endDate) | endDateEntry.isBefore(startDate))) {
+                if (!isAvailable(startDate, endDate, startDateEntry, endDateEntry)) {
                     availableRoomIds.add(rs.getString("roomId"));
                 }
             }
@@ -67,10 +123,10 @@ public class Hotel extends Participant {
     }
 
     public boolean isAvailable(LocalDate startDate, LocalDate endDate, LocalDate startDateEntry, LocalDate endDateEntry){
-        if (!(startDateEntry.isAfter(endDate) | endDateEntry.isBefore(startDate))) {
-            return false;
+        if (startDateEntry.isAfter(endDate) | endDateEntry.isBefore(startDate)) {
+            return true;
         }
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
