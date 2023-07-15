12828f5b3d11774c3e10798bbbbc15aa5bef897e
Nico1808 <niconiebisch@gmail.com>
2023-07-15 02:44:41 +0200

    latenight pagnia phirania session

--- src/main/java/org/hotel/Execute.java
+++ src/main/java/org/hotel/Execute.java
@@ -14,18 +14,28 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Execute {
+
+    private final static Logger LOGGER = Logger.getLogger(Execute.class.getName());
+
+    private static TransactionContext transactionContext;
+
+    private static Map<UUID, TransactionContext> transactionContextMap = new HashMap<>();
+
+    private static LogWriter<TransactionContext> logWriter = new LogWriter<>();
+
     public static void main(String[] args) {
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
 
         Hotel h = new Hotel();
-        LocalDate startDate = LocalDate.of(2023, 8, 1);
-        LocalDate endDate = LocalDate.of(2023, 8, 14);
-        h.getAvailableItems(startDate, endDate);
 
         while (true) {
             try {
@@ -40,67 +50,115 @@ public class Execute {
                 //response UDPMessage
                 UDPMessage responeseMessage;
 
-                System.out.println("Listening on Port " + Participant.hotelPort);
+                LOGGER.log(Level.INFO, "Hotel listening on Port: " + Participant.hotelPort);
                 h.dgSocket.receive(dgPacketIn);
-                System.out.println("recieved aber trd im arsch: ");
 
                 //string data to parse it into Objects
                 String data = new String(dgPacketIn.getData(), 0, dgPacketIn.getLength());
                 //parsed UDP message
                 UDPMessage dataObject = objectMapper.readValue(data, UDPMessage.class);
-                System.out.println(dataObject.getOperation());
+
+                //store TransaktionID for context on further processing
+                UUID transactionId = dataObject.getTransaktionNumber();
+
+                //store originPort of the requesting travelBroker
+                int originPort = dataObject.getOriginPort();
 
                 switch (dataObject.getOperation()){
                     case PREPARE -> {
-                        System.out.println("prepare recieved");
+                        //log prepare recieved but not answered
+                        transactionContext = new TransactionContext(States.PREPARE, originPort, false);
+                        logWriter.write(transactionId, transactionContext);
+                        transactionContextMap.put(transactionId, transactionContext);
+
+                        LOGGER.log(Level.INFO, "2PC: Prepare - " + transactionId);
+
+                        //get Data of Message
                         messageData = dataObject.getData();
                         data = new String(messageData, 0, messageData.length);
                         BookingData bookingData = objectMapper.readValue(data, BookingData.class);
 
+                        //run actual prepare and safe the response (Commit or Abort)
                         Operations response = h.prepare(bookingData, dataObject.getTransaktionNumber());
-                        responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), new byte[0], SendingInformation.HOTEL, response);
+                        responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.HOTEL, response);
 
-                        //send response back to TravelBroker
+                        //send response back to corresponding TravelBroker instance
                         messageData = objectMapper.writeValueAsBytes(responeseMessage);
-                        DatagramPacket dgOutPrepare = new DatagramPacket(messageData, messageData.length, Participant.localhost, Participant.travelBrokerPort);
+                        DatagramPacket dgOutPrepare = new DatagramPacket(messageData, messageData.length, Participant.localhost, originPort);
 
                         h.dgSocket.send(dgOutPrepare);
-                        System.out.println("prepare answered: " + response);
+
+                        //log prepare answered
+                        transactionContext = new TransactionContext(States.PREPARE, originPort, true);
+                        logWriter.write(transactionId, transactionContext);
+                        transactionContextMap.put(transactionId, transactionContext);
                     }
                     case COMMIT -> {
-                        System.out.println("commit recieved");
-                        if(h.commit(dataObject.getTransaktionNumber())) {
-                            responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), new byte[0], SendingInformation.HOTEL, Operations.OK);
-                            parsedMessage = objectMapper.writeValueAsBytes(responeseMessage);
-                            DatagramPacket dgOutCommit = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, Participant.travelBrokerPort);
+                        //log commit recieved but not answered
+                        transactionContext = new TransactionContext(States.COMMIT, originPort, false);
+                        logWriter.write(transactionId, transactionContext);
+                        transactionContextMap.put(transactionId, transactionContext);
 
+                        LOGGER.log(Level.INFO, "2PC: Commit - " + transactionId);
+
+                        //run actual commit and check if transaction was completed successfully if not, no answer is send
+                        if(h.commit(dataObject.getTransaktionNumber())) {
+                            //create and parse response
+                            responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.HOTEL, Operations.OK);
+                            parsedMessage = objectMapper.writeValueAsBytes(responeseMessage);
+
+                            //send response to corresponding travelBroker instance
+                            DatagramPacket dgOutCommit = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
                             h.dgSocket.send(dgOutCommit);
-                            System.out.println("commit answered - Ok");
+
+                            //log commit answered
+                            transactionContext = new TransactionContext(States.COMMIT, originPort, true);
+                            logWriter.write(transactionId, transactionContext);
+                            transactionContextMap.put(transactionId, transactionContext);
                         }
                     }
                     case ABORT -> {
-                        System.out.println("abort recieved");
-                        if(h.abort(dataObject.getTransaktionNumber())){
-                            responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), new byte[0], SendingInformation.HOTEL, Operations.OK);
-                            parsedMessage = objectMapper.writeValueAsBytes(responeseMessage);
-                            DatagramPacket dgOutAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, Participant.travelBrokerPort);
+                        //log abort but not answered
+                        transactionContext = new TransactionContext(States.ABORT, originPort, false);
+                        logWriter.write(transactionId, transactionContext);
+                        transactionContextMap.put(transactionId, transactionContext);
 
+                        LOGGER.log(Level.INFO, "2PC: Abort - " + transactionId);
+
+                        //run actual abort and check if aborted successfully
+                        if(h.abort(dataObject.getTransaktionNumber())){
+                            //prepare answer
+                            responeseMessage = new UDPMessage(dataObject.getTransaktionNumber(), SendingInformation.HOTEL, Operations.OK);
+                            parsedMessage = objectMapper.writeValueAsBytes(responeseMessage);
+
+                            //send response to corresponding travelBroker instance
+                            DatagramPacket dgOutAbort = new DatagramPacket(parsedMessage, parsedMessage.length, Participant.localhost, originPort);
                             h.dgSocket.send(dgOutAbort);
-                            System.out.println("abort answered - OK");
+
+                            //log abort answered
+                            transactionContext = new TransactionContext(States.ABORT, originPort, true);
+                            logWriter.write(transactionId, transactionContext);
+                            transactionContextMap.put(transactionId, transactionContext);
                         }
                     }
                     case AVAILIBILITY -> {
+                        LOGGER.log(Level.INFO, "Availability: request from travelBroker");
+
+                        //parse availability data back to class
                         messageData = dataObject.getData();
                         AvailabilityData availabilityData = objectMapper.readValue(messageData, AvailabilityData.class);
 
+                        //run actual availability check with requestet params and store available Rooms
                         ArrayList<Object> availableItems = h.getAvailableItems(availabilityData.getStartDate(), availabilityData.getEndDate());
-                        System.out.println(availableItems.get(0));
+
+                        //if rooms not null prepare data and send answer
                         if(!(availableItems == null)){
                             byte[] parsedItems = objectMapper.writeValueAsBytes(availableItems);
-                            UDPMessage responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), parsedItems, SendingInformation.HOTEL, Operations.AVAILIBILITY);
+                            UDPMessage responseMessage = new UDPMessage(dataObject.getTransaktionNumber(), parsedItems, SendingInformation.HOTEL, Operations.AVAILIBILITY, Participant.hotelPort);
                             parsedMessage = objectMapper.writeValueAsBytes(responseMessage);
-                            //Datagrampacket for sending the response
-                            DatagramPacket dgOutAvailability= new DatagramPacket(parsedMessage, parsedMessage.length, h.localhost, h.travelBrokerPort);
+
+                            //send response to corresponding travelBroker instance
+                            DatagramPacket dgOutAvailability= new DatagramPacket(parsedMessage, parsedMessage.length, h.localhost, originPort);
                             h.dgSocket.send(dgOutAvailability);
 
                         }
@@ -108,7 +166,7 @@ public class Execute {
                 }
 
             } catch (Exception e) {
-
+                LOGGER.log(Level.SEVERE, "The Socket or the objectMapper threw an error", e);
             }
         }
     }
--- src/main/java/org/hotel/Hotel.java
+++ src/main/java/org/hotel/Hotel.java
@@ -12,16 +12,20 @@ import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Hotel extends Participant {
 
     public DatagramSocket dgSocket;
 
+    private final static Logger LOGGER = Logger.getLogger(Execute.class.getName());
+
     public Hotel() {
         try {
             dgSocket = new DatagramSocket(Participant.hotelPort);
         } catch(Exception e){
-            System.out.println("Socket was not set: " + e.getMessage());
+            LOGGER.log(Level.SEVERE, "Socket was not set", e);
         }
     }
 
@@ -38,26 +42,28 @@ public class Hotel extends Participant {
             PreparedStatement stm = con.prepareStatement("SELECT * FROM booking");
             ResultSet rs = stm.executeQuery();
 
+            //loop through responded table and check if rooms are available or not
             while (rs.next()) {
                 if(rs.getInt("roomId") == requestedId){
                     LocalDate startDateEntry = LocalDate.parse(rs.getString("startDate"));
                     LocalDate endDateEntry = LocalDate.parse(rs.getString("endDate"));
                     if (!isAvailable(startDate, endDate, startDateEntry, endDateEntry)) {
+                        //if not available return abort
                         return Operations.ABORT;
                     }
 
                 }
             }
+
             //Room is available
             String query = "INSERT INTO booking VALUES (\"" + transaktionId + "\", \"" + startDate + "\", \"" + endDate + "\", 0, " + requestedId + ")";
             System.out.println(query);
             stm = con.prepareStatement(query);
             stm.executeUpdate();
-            System.out.println("successfully booked");
 
             return Operations.READY;
         }catch (Exception e){
-            System.out.println("kapput");
+            LOGGER.log(Level.SEVERE, "Something went wrong with the database connection", e);
             return Operations.ABORT;
         }
 
@@ -68,11 +74,12 @@ public class Hotel extends Participant {
         DatabaseConnection dbConn = new DatabaseConnection();
         try(Connection con = dbConn.getConn()){
             PreparedStatement stm = con.prepareStatement("UPDATE booking SET stable = 1 WHERE bookingID = \"" + transaktionId + "\"");
-            System.out.println("UPDATE booking SET stable = 1 WHERE id = \"" + transaktionId + "\"");
             stm.executeUpdate();
+            //return true since the statement was successfully
             return true;
         }catch(Exception e){
-            System.out.println("des kann jetzt nicht Wahrsteiner im Commit?: " + e.getMessage());
+            LOGGER.log(Level.SEVERE, "Something went wrong with the database connection", e);
+            //return false since the transaction wasn't succesfully set stable
             return false;
         }
     }
@@ -82,12 +89,13 @@ public class Hotel extends Participant {
         DatabaseConnection dbConn = new DatabaseConnection();
         try(Connection con = dbConn.getConn()){
             String s = "DELETE FROM booking WHERE bookingID = \"" + transaktionId + "\"";
-            System.out.println(s);
             PreparedStatement stm = con.prepareStatement(s);
             stm.executeUpdate();
+            //return true since the statement was succesfully
             return true;
         }catch(Exception e){
-            System.out.println("des kann jetzt nicht Wahrsteiner im Abort?");
+            LOGGER.log(Level.SEVERE, "Something went wrong with the database connection", e);
+            //return false since the transaction wasn't succesfully aborted
             return false;
         }
     }
@@ -97,20 +105,22 @@ public class Hotel extends Participant {
     public ArrayList<Object> getAvailableItems(LocalDate startDate, LocalDate endDate) {
         DatabaseConnection dbConn = new DatabaseConnection();
         ArrayList<String> availableRoomIds = new ArrayList<>();
-        ResultSet rs = null;
-        UDPMessage udpMessage;
+        ResultSet rs;
         try(Connection con = dbConn.getConn()){
             PreparedStatement stm = con.prepareStatement("SELECT * FROM booking");
             rs = stm.executeQuery();
 
+            //loop through result table and check if rooms are available or not
             while(rs.next()){
                 LocalDate startDateEntry = LocalDate.parse(rs.getString("startDate"));
                 LocalDate endDateEntry = LocalDate.parse(rs.getString("endDate"));
                 if (!isAvailable(startDate, endDate, startDateEntry, endDateEntry)) {
+                    //add room if not available to exclude in query later
                     availableRoomIds.add(rs.getString("roomId"));
                 }
             }
 
+            //build string to be in right format for the sql query
             String availableRoomsIds = "";
             for (int i = 0; i < availableRoomIds.size(); i++) {
                 if( i < availableRoomIds.size() - 1){
@@ -121,9 +131,12 @@ public class Hotel extends Participant {
             }
 
             stm = con.prepareStatement("SELECT * FROM room WHERE roomID NOT IN (?)");
+            //append prepared string at the position of "?"
             stm.setString(1, availableRoomsIds);
             rs = stm.executeQuery();
             ArrayList<Object> availableRooms = new ArrayList<>();
+
+            //add all available rooms to returned table
             while(rs.next()){
                 availableRooms.add(new Room(rs.getInt("roomID"), rs.getString("name")));
             }
@@ -131,20 +144,18 @@ public class Hotel extends Participant {
             return availableRooms;
 
         }catch(Exception e){
-            System.out.println("An Error has occured: " + e.getMessage());
+            LOGGER.log(Level.SEVERE, "Something went wrong with the database connection", e);
+            //return null since there couldn't be any rooms selected
             return null;
         }
     }
 
+    //method for comparing two timeframes
     public boolean isAvailable(LocalDate startDate, LocalDate endDate, LocalDate startDateEntry, LocalDate endDateEntry){
         if (startDateEntry.isAfter(endDate) | endDateEntry.isBefore(startDate)) {
             return true;
         }
         return false;
     }
-
-    public static void main(String[] args) {
-
-        }
 }
 
